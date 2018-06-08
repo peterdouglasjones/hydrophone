04d0e65cb7627f321408e1346bccbc92c1f6a136
peterdouglasjones <peterdouglasjones@gmail.com>
2018-06-08 14:45:16 -0700

    v1.1 with 0400 new
    Adds new 0400 hydrophone, adds straight conversions between P and I

--- hydrophone.m
+++ hydrophone.m
@@ -5,8 +5,8 @@ classdef hydrophone < matlab.apps.AppBase
         UIFigure              matlab.ui.Figure                   % Hydropho...
         HPmodel               matlab.ui.container.ButtonGroup    % Hydropho...
         HPmodel0200           matlab.ui.control.RadioButton      % HNP-0200
-        HPmodel0400           matlab.ui.control.RadioButton      % HNP-0400
-        HPmodel0500           matlab.ui.control.RadioButton      % HNR-0500
+        HPmodel0400old        matlab.ui.control.RadioButton      % HNP-0400...
+        HPmodel0400new        matlab.ui.control.RadioButton      % HNP-0400...
         DCBgain               matlab.ui.container.ButtonGroup    % DC Block...
         gainvalhigh           matlab.ui.control.RadioButton      % High
         gainvallow            matlab.ui.control.RadioButton      % Low
@@ -22,20 +22,23 @@ classdef hydrophone < matlab.apps.AppBase
         input_array_var       matlab.ui.control.EditField       
         Label2                matlab.ui.control.Label            % Input pr...
         CheckBox              matlab.ui.control.CheckBox         % Print ou...
+        HPmodel0500           matlab.ui.control.RadioButton      % HNP-0500
+        PtoI                  matlab.ui.control.RadioButton      % Convert ...
+        ItoP                  matlab.ui.control.RadioButton      % Convert ...
     end
 
     methods (Access = private)
 
         % Code that executes after component creation
         function startupFcn(app)
-            
+
         end
 
         % execute_button button pushed function
         function execute_buttonButtonPushed(app)
             
             %{
-Instructions: edit as mlapp in matlab "appdesigner". Use mlapp2classdef.m to
+Instructions for code editing: edit as mlapp in matlab "appdesigner". Use mlapp2classdef.m to
 convert to .m file after each update. Upload both files to GitHub.
             
 Combines previously developed calculations in HydroCalcSN042013Rev2016.m
@@ -55,9 +58,6 @@ Version 18.02.01
 
 to do:
 field for output on the gui
-combine P and I calcs
-print to CSV or something
-clean up the code, use the while loops for everything
 
 %}
             fprintf('\n');
@@ -71,9 +71,12 @@ clean up the code, use the while loops for everything
             if app.HPmodel0500.Value;
                 hpvals = dlmread('SenslookupHNR0500.txt','',1,0);
                 hydrophone_model = 'HNR-0500';
-            elseif app.HPmodel0400.Value;
+            elseif app.HPmodel0400old.Value;
                 hpvals = dlmread('SenslookupHNP0400.txt','',1,0);
-                hydrophone_model = 'HNP-0400';
+                hydrophone_model = 'HNP-0400 (old)';
+            elseif app.HPmodel0400new.Value;
+                hpvals = dlmread('SenslookupHNP0400-1430.txt','',1,0);
+                hydrophone_model = 'HNP-0400 (new)';
             elseif app.HPmodel0200.Value;
                 hpvals = dlmread('SenslookupHNP0200.txt','',1,0);
                 hydrophone_model = 'HNP-0200';
@@ -116,37 +119,39 @@ clean up the code, use the while loops for everything
             
             %Pull CAP_PREAMP and GAIN_DB for either high, low, or no gain.
             if app.gainvallow.Value;
-                gaintext = ['"Low'];
+                gaintext = ['Low'];
                 capamp = interp1(pavals(:,1),pavals(:,7),freq_in_mhz_p,'linear');
                 gaindB = interp1(pavals(:,1),pavals(:,5),freq_in_mhz_p,'linear');
                 gain=10.^(gaindB./20); %converting dB to amplitude
                 ml=mc.*gain.*caphydro./(caphydro+capamp);
                 kcalfactor=zacoustic.*(ml).^2;
             elseif app.gainvalhigh.Value;
-                gaintext = ['"High'];
+                gaintext = ['High'];
                 capamp = interp1(pavals(:,1),pavals(:,4),freq_in_mhz_p,'linear');
                 gaindB = interp1(pavals(:,1),pavals(:,2),freq_in_mhz_p,'linear');
                 gain=10.^(gaindB./20); %converting dB to amplitude
                 ml=mc.*gain.*caphydro./(caphydro+capamp);
                 kcalfactor=zacoustic.*(ml).^2;
             elseif app.gainvalnone.Value;
-                gaintext = ['"No'];
+                gaintext = ['No'];
                 capamp = interp1(pavals(:,1),pavals(:,4),freq_in_mhz_p,'linear');
                 gaindB = interp1(pavals(:,1),pavals(:,2),freq_in_mhz_p,'linear');
-                kcalfactor = zacoustic.*(mc).^2;
                 ml = mc;
+                kcalfactor = zacoustic.*(mc).^2;
             end
             
             arrayflag = app.CheckBox.Value;
             
-            fprintf('\n');
-            fprintf('--------------------------------------------------------------------\n');
-            fprintf(' %s gain" calibration values for %s hydrophone at %3.2f MHz\n',gaintext,hydrophone_model,freq_in_mhz);
-            fprintf(' Hydrophone capacitance: %.3f pF\n',caphydro);
-            fprintf(' Hydrophone EOC Sensitivity: %e V/Pa\n',mc);
-            fprintf(' Kcalfactor = %e V²cm²/W\n',kcalfactor);
-            fprintf(' ML Calibrated Pressure Sensitivity: %e V/Pa (peak)\n',ml);
-            fprintf('--------------------------------------------------------------------\n');
+            if app.PtoV.Value || app.ItoV.Value || app.VtoP.Value || app.VtoI.Value;
+                fprintf('\n');
+                fprintf('--------------------------------------------------------------------------\n');
+                fprintf(' %s gain calibration values for %s hydrophone at %3.2f MHz\n',gaintext,hydrophone_model,freq_in_mhz);
+                fprintf(' Hydrophone capacitance: %.3f pF\n',caphydro);
+                fprintf(' MC Hydrophone EOC Sensitivity: %e V/Pa, peak\n',mc);
+                fprintf(' ML Calibrated Pressure Sensitivity: %e V/Pa, peak\n',ml);
+                fprintf(' Kcalfactor = %e V²cm²/W\n',kcalfactor);
+                fprintf('--------------------------------------------------------------------------\n');
+            end
             
             if app.PtoV.Value;
                 i = 1;
@@ -160,9 +165,8 @@ clean up the code, use the while loops for everything
                     fprintf('   %6.4g MPa = %.1f mVpp\n',pressureval,mVpp);
                     i = i+1;
                 end
-                if arrayflag;
-                    Pressures_in_MPa = input_array
-                    Voltages_in_mVpp = output_array
+                if app.CheckBox.Value;
+                    MPa_vs_mVpp = [transpose(input_array),transpose(output_array)]
                 end
                 fprintf('\n');
                 
@@ -178,9 +182,8 @@ clean up the code, use the while loops for everything
                     fprintf('   %6.4g W/cm² = %.1f mVpp\n',intensityval,mVpp);
                     i = i+1;
                 end
-                if arrayflag;
-                    Intensities_in_Wattspercm2 = input_array
-                    Voltages_in_mVpp = output_array
+                if app.CheckBox.Value;
+                    Wattspercm2_vs_mVpp = [transpose(input_array),transpose(output_array)]
                 end
                 fprintf('\n');
                 
@@ -197,9 +200,8 @@ clean up the code, use the while loops for everything
                     fprintf('   %6.1f mVpp = %.4g MPa\n',mVpp,Pressure);
                     i = i+1;
                 end
-                if arrayflag;
-                    mVpp = input_array
-                    Pressures_in_MPa = output_array
+                if app.CheckBox.Value;
+                    mVpp_vs_MPa = [transpose(input_array),transpose(output_array)]
                 end
                 fprintf('\n');
                 
@@ -216,9 +218,44 @@ clean up the code, use the while loops for everything
                     fprintf('   %6.1f mVpp = %.4g W/cm²\n',mVpp,Intensity);
                     i = i+1;
                 end     
-                if arrayflag;
-                    mVpp = input_array
-                    Intensities_in_Wattspercm2 = output_array
+                if app.CheckBox.Value;
+                    mVpp_vs_Wattspercm2 = [transpose(input_array),transpose(output_array)]
+                end
+                fprintf('\n');
+            
+            elseif app.PtoI.Value;
+                fprintf('-------------------------------------------\n');
+                fprintf(' Convert Pressures to Intensities\n');
+                fprintf('-------------------------------------------\n');
+                i = 1;
+                output_array = [];
+                while i <= length(input_array);
+                    pressureval = input_array(i);
+                    intensityval = ((input_array(i)*1e6).^2)/(10000*2*1500*1000); %convert from pressure to intensity
+                    output_array(i) = intensityval;
+                    fprintf('   %6.4g MPa = %.4g W/cm²\n',pressureval,intensityval);
+                    i = i+1;
+                end
+                if app.CheckBox.Value;
+                    MPa_vs_mVpp = [transpose(input_array),transpose(output_array)]
+                end
+                fprintf('\n');
+                
+            elseif app.ItoP.Value;
+                fprintf('-------------------------------------------\n');
+                fprintf(' Convert Intensities to Pressures\n');
+                fprintf('-------------------------------------------\n');
+                i = 1;
+                output_array = [];
+                while i <= length(input_array);
+                    intensityval = input_array(i);
+                    pressureval = sqrt(intensityval.*(10000*2*1500*1000))./(1e6);
+                    output_array(i) = pressureval;
+                    fprintf('   %6.4g W/cm² = %.4g MPa\n',intensityval,pressureval);
+                    i = i+1;
+                end
+                if app.CheckBox.Value;
+                    MPa_vs_mVpp = [transpose(input_array),transpose(output_array)]
                 end
                 fprintf('\n');
                 
@@ -232,11 +269,14 @@ clean up the code, use the while loops for everything
             selectedButton1 = app.HPmodel.SelectedObject;
             
             if app.HPmodel0200.Value;
-                set(app.gainvalnone,'enable','off');
                 app.gainvalhigh.Value = 1;
-            elseif app.HPmodel0400.Value;
                 set(app.gainvalnone,'enable','off');
+            elseif app.HPmodel0400old.Value;
                 app.gainvalhigh.Value = 1;
+                set(app.gainvalnone,'enable','off');
+            elseif app.HPmodel0400new.Value;
+                app.gainvalhigh.Value = 1;
+                set(app.gainvalnone,'enable','off');
             elseif app.HPmodel0500.Value;
                 set(app.gainvalnone,'enable','on');
             end
@@ -267,6 +307,10 @@ clean up the code, use the while loops for everything
                 app.Label2.Text = 'Input voltages in mVpp, comma-separated:';
             elseif app.VtoI.Value;
                 app.Label2.Text = 'Input voltages in mVpp, comma-separated:';
+            elseif app.PtoI.Value;
+                app.Label2.Text = 'Input pressures in MPa, comma-separated:';
+            elseif app.ItoP.Value;
+                app.Label2.Text = 'Input intensities in W/cm², comma-separated:';
             end
             
         end
@@ -282,6 +326,16 @@ clean up the code, use the while loops for everything
             value = app.CheckBox.Value;
             arrayflag = app.CheckBox.Value;
         end
+
+        % input_array_var value changed function
+        function input_array_varValueChanged(app)
+            value8 = app.input_array_var.Value;
+            
+%             currChar = get(handles.UIFigure,'CurrentCharacter');
+%             if isequal(currChar,char(13)) %char(13) == enter key
+%                 fprinf('k');
+%             end
+        end
     end
 
     % App initialization and construction
@@ -292,8 +346,8 @@ clean up the code, use the while loops for everything
 
             % Create UIFigure
             app.UIFigure = uifigure;
-            app.UIFigure.Position = [100 100 367 449];
-            app.UIFigure.Name = 'Hydrophone V/P/I Conversions';
+            app.UIFigure.Position = [100 100 359 520];
+            app.UIFigure.Name = 'Hydrophone V/P/I Conversions v1.1';
             setAutoResize(app, app.UIFigure, true)
 
             % Create HPmodel
@@ -305,23 +359,28 @@ clean up the code, use the while loops for everything
             app.HPmodel.FontUnits = 'pixels';
             app.HPmodel.FontSize = 12;
             app.HPmodel.Units = 'pixels';
-            app.HPmodel.Position = [50 321 123 106];
+            app.HPmodel.Position = [46 368 136 130];
 
             % Create HPmodel0200
             app.HPmodel0200 = uiradiobutton(app.HPmodel);
             app.HPmodel0200.Text = 'HNP-0200';
-            app.HPmodel0200.Position = [10 59 77 16];
+            app.HPmodel0200.Position = [10 83 77 16];
 
-            % Create HPmodel0400
-            app.HPmodel0400 = uiradiobutton(app.HPmodel);
-            app.HPmodel0400.Value = true;
-            app.HPmodel0400.Text = 'HNP-0400';
-            app.HPmodel0400.Position = [10 37 77 16];
+            % Create HPmodel0400old
+            app.HPmodel0400old = uiradiobutton(app.HPmodel);
+            app.HPmodel0400old.Value = true;
+            app.HPmodel0400old.Text = 'HNP-0400 (old)';
+            app.HPmodel0400old.Position = [10 61 105 16];
+
+            % Create HPmodel0400new
+            app.HPmodel0400new = uiradiobutton(app.HPmodel);
+            app.HPmodel0400new.Text = 'HNP-0400 (new)';
+            app.HPmodel0400new.Position = [10 39 111 16];
 
             % Create HPmodel0500
             app.HPmodel0500 = uiradiobutton(app.HPmodel);
-            app.HPmodel0500.Text = 'HNR-0500';
-            app.HPmodel0500.Position = [10 15 78 16];
+            app.HPmodel0500.Text = 'HNP-0500';
+            app.HPmodel0500.Position = [10 16 77 16];
 
             % Create DCBgain
             app.DCBgain = uibuttongroup(app.UIFigure);
@@ -332,7 +391,7 @@ clean up the code, use the while loops for everything
             app.DCBgain.FontUnits = 'pixels';
             app.DCBgain.FontSize = 12;
             app.DCBgain.Units = 'pixels';
-            app.DCBgain.Position = [190 321 123 106];
+            app.DCBgain.Position = [192 380 123 106];
 
             % Create gainvalhigh
             app.gainvalhigh = uiradiobutton(app.DCBgain);
@@ -354,14 +413,14 @@ clean up the code, use the while loops for everything
             % Create LabelNumericEditField
             app.LabelNumericEditField = uilabel(app.UIFigure);
             app.LabelNumericEditField.HorizontalAlignment = 'right';
-            app.LabelNumericEditField.Position = [76 279 94 15];
+            app.LabelNumericEditField.Position = [76 323 94 15];
             app.LabelNumericEditField.Text = 'Frequency (MHz)';
 
             % Create freq_in_mhz_input
             app.freq_in_mhz_input = uieditfield(app.UIFigure, 'numeric');
             app.freq_in_mhz_input.ValueChangedFcn = createCallbackFcn(app, @freq_in_mhz_inputValueChanged);
             app.freq_in_mhz_input.Limits = [0 200];
-            app.freq_in_mhz_input.Position = [185 275 100 22];
+            app.freq_in_mhz_input.Position = [185 319 100 22];
             app.freq_in_mhz_input.Value = 1;
 
             % Create conversion_type
@@ -372,51 +431,62 @@ clean up the code, use the while loops for everything
             app.conversion_type.FontUnits = 'pixels';
             app.conversion_type.FontSize = 12;
             app.conversion_type.Units = 'pixels';
-            app.conversion_type.Position = [32 162 301 103];
+            app.conversion_type.Position = [32 166 301 150];
 
             % Create PtoV
             app.PtoV = uiradiobutton(app.conversion_type);
             app.PtoV.Value = true;
             app.PtoV.Text = 'Determine voltages for desired peak pressures';
-            app.PtoV.Position = [10 76 273 16];
+            app.PtoV.Position = [10 123 273 16];
 
             % Create ItoV
             app.ItoV = uiradiobutton(app.conversion_type);
             app.ItoV.Text = 'Determine voltages for desired intensities';
-            app.ItoV.Position = [10 54 301.109375 16];
+            app.ItoV.Position = [10 101 301.109375 16];
 
             % Create VtoP
             app.VtoP = uiradiobutton(app.conversion_type);
             app.VtoP.Text = 'Calculate peak pressures from voltage readings';
-            app.VtoP.Position = [10 32 279 16];
+            app.VtoP.Position = [10 79 279 16];
 
             % Create VtoI
             app.VtoI = uiradiobutton(app.conversion_type);
             app.VtoI.Text = 'Calculate intensities from voltage readings';
-            app.VtoI.Position = [10 9 301.453125 16];
+            app.VtoI.Position = [10 56 301.453125 16];
+
+            % Create PtoI
+            app.PtoI = uiradiobutton(app.conversion_type);
+            app.PtoI.Text = 'Convert pressures to intensities';
+            app.PtoI.Position = [10 33 190 16];
+
+            % Create ItoP
+            app.ItoP = uiradiobutton(app.conversion_type);
+            app.ItoP.Text = 'Convert intensities to pressures';
+            app.ItoP.Position = [10 11 190 16];
 
             % Create execute_button
             app.execute_button = uibutton(app.UIFigure, 'push');
             app.execute_button.ButtonPushedFcn = createCallbackFcn(app, @execute_buttonButtonPushed);
-            app.execute_button.Position = [131 35 100 22];
+            app.execute_button.Position = [131 37 100 22];
             app.execute_button.Text = 'Calculate';
 
             % Create input_array_var
             app.input_array_var = uieditfield(app.UIFigure, 'text');
-            app.input_array_var.Position = [56.03125 84 256 22];
+            app.input_array_var.ValueChangedFcn = createCallbackFcn(app, @input_array_varValueChanged);
+            app.input_array_var.Position = [42.03125 107 273 22];
 
             % Create Label2
             app.Label2 = uilabel(app.UIFigure);
             app.Label2.HorizontalAlignment = 'center';
-            app.Label2.Position = [56 108 255 15];
+            app.Label2.Position = [52 130 255 15];
             app.Label2.Text = 'Input pressures in MPa, comma-separated:';
 
             % Create CheckBox
             app.CheckBox = uicheckbox(app.UIFigure);
             app.CheckBox.ValueChangedFcn = createCallbackFcn(app, @CheckBoxValueChanged);
-            app.CheckBox.Text = 'Print output arrays for copy/paste';
+            app.CheckBox.Text = 'Print output array for copy/paste';
             app.CheckBox.FontSize = 9;
-            app.CheckBox.Position = [101 147 159 16];
+            app.CheckBox.Position = [107 87 154 16];
         end
     end
 
